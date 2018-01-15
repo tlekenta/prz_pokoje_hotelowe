bffc6eb3adf159461e71fb8b94e16ef119f8fba6
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-15 14:45:28 +0100

    Dodanie mockowych rezerwacji do bazy

--- src/main/java/pl/edu/wat/InitDatabase.java
+++ src/main/java/pl/edu/wat/InitDatabase.java
@@ -2,32 +2,54 @@ package pl.edu.wat;
 
 import javafx.application.Preloader;
 import javafx.stage.Stage;
+import pl.edu.wat.model.dao.ReservationsDAO;
 import pl.edu.wat.model.dao.RoomDAO;
+import pl.edu.wat.model.entities.Reservation;
 import pl.edu.wat.model.entities.Room;
+
+import java.time.LocalDate;
 
 public class InitDatabase extends Preloader {
     RoomDAO roomDAO = new RoomDAO();
+    ReservationsDAO reservationsDAO = new ReservationsDAO();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Room room = new Room();
-        room.setNumber("1");
-        room.setNumberOfPersons(2);
-        room.setNumberOfBeds(1);
-        roomDAO.save(room);
+        Room room1 = new Room();
+        room1.setNumber("1");
+        room1.setNumberOfPersons(2);
+        room1.setNumberOfBeds(1);
+        roomDAO.save(room1);
 
-        room = new Room();
-        room.setNumber("2");
-        room.setNumberOfPersons(3);
-        room.setNumberOfBeds(2);
-        roomDAO.save(room);
+        Room room2 = new Room();
+        room2.setNumber("2");
+        room2.setNumberOfPersons(3);
+        room2.setNumberOfBeds(2);
+        roomDAO.save(room2);
 
 
-        room = new Room();
-        room.setNumber("3");
-        room.setNumberOfPersons(4);
-        room.setNumberOfBeds(2);
-        roomDAO.save(room);
+        Room room3 = new Room();
+        room3.setNumber("3");
+        room3.setNumberOfPersons(4);
+        room3.setNumberOfBeds(2);
+        roomDAO.save(room3);
 
+        Reservation reservation1 = new Reservation();
+        reservation1.setDateFrom(LocalDate.of(2018, 2, 1));
+        reservation1.setDateTo(LocalDate.of(2018, 2, 8));
+        reservation1.setRoom(room1);
+        reservationsDAO.save(reservation1);
+
+        Reservation reservation2 = new Reservation();
+        reservation2.setDateFrom(LocalDate.of(2018, 1, 16));
+        reservation2.setDateTo(LocalDate.of(2018, 1, 28));
+        reservation2.setRoom(room2);
+        reservationsDAO.save(reservation2);
+
+        Reservation reservation3 = new Reservation();
+        reservation3.setDateFrom(LocalDate.of(2018, 1, 14));
+        reservation3.setDateTo(LocalDate.of(2018, 1, 21));
+        reservation3.setRoom(room3);
+        reservationsDAO.save(reservation3);
     }
 }
