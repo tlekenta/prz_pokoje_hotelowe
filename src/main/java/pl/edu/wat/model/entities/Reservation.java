a29947e8800b135dec4581804de9c5361e1e83c8
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-23 20:22:18 +0100

    Zmiana sekwencji w encjach na IDENTITY

--- src/main/java/pl/edu/wat/model/entities/Reservation.java
+++ src/main/java/pl/edu/wat/model/entities/Reservation.java
@@ -2,17 +2,14 @@ package pl.edu.wat.model.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
 public class Reservation {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDate dateFrom;
