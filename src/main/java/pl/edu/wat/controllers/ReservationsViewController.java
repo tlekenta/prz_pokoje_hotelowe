ddb63b1be48e466470a6214d9852fa8f7304473f
Tomasz Lekenta <lekenta95@gmail.com>
2018-01-24 19:11:15 +0100

    Zmiany w widokach tabel
    Wartość kolumny ceny wyliczana na podstawie obecnej waluty

--- src/main/java/pl/edu/wat/controllers/ReservationsViewController.java
+++ src/main/java/pl/edu/wat/controllers/ReservationsViewController.java
@@ -1,6 +1,8 @@
 package pl.edu.wat.controllers;
 
+import javafx.beans.property.ReadOnlyDoubleWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -8,12 +10,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import pl.edu.wat.ApplicationSettingsReader;
 import pl.edu.wat.model.entities.Reservation;
 import pl.edu.wat.model.services.ReservationService;
+import pl.edu.wat.web.CurrencyService;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class ReservationsViewController implements Initializable {
 
@@ -30,17 +37,25 @@ public class ReservationsViewController implements Initializable {
     TableColumn<Reservation, String> roomNumberColumn;
 
     @FXML
-    TableColumn<Reservation, Double> priceColumn;
+    TableColumn<Reservation, String> priceColumn;
 
     private ReservationService reservationService = ReservationService.getInstance();
+
+    private ApplicationSettingsReader asr = new ApplicationSettingsReader();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         roomNumberColumn.setCellValueFactory((cellData) ->
                 new ReadOnlyStringWrapper(cellData.getValue().getRoom().getNumber())
         );
-        priceColumn.setCellValueFactory(
-                new PropertyValueFactory<>("totalPrice")
+        priceColumn.setCellValueFactory((cellData) -> {
+                double price =
+                        cellData.getValue().getRoom().getPricePerNight()
+                                * DAYS.between(cellData.getValue().getDateFrom(), cellData.getValue().getDateTo())
+                                / CurrencyService.getCurrnecyPrice(asr.getCurrnecy());
+                DecimalFormat df = new DecimalFormat("#.##");
+                return new ReadOnlyStringWrapper(df.format(price) + " " + asr.getCurrnecy());
+            }
         );
 
         reservationService.getReservationsList()
--- src/main/java/pl/edu/wat/controllers/RoomsViewController.java
+++ src/main/java/pl/edu/wat/controllers/RoomsViewController.java
@@ -1,5 +1,7 @@
 package pl.edu.wat.controllers;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -7,10 +9,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import pl.edu.wat.ApplicationSettingsReader;
 import pl.edu.wat.model.entities.Room;
 import pl.edu.wat.model.services.RoomService;
+import pl.edu.wat.web.CurrencyService;
 
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 public class RoomsViewController implements Initializable {
@@ -18,14 +23,19 @@ public class RoomsViewController implements Initializable {
     @FXML TableColumn<Room, String> numberColumn;
     @FXML TableColumn<Room, Integer> numberOfPersonsColumn;
     @FXML TableColumn<Room, Integer> numberOfBedsColumn;
-    @FXML TableColumn<Room, Double> priceColumn;
+    @FXML TableColumn<Room, String> priceColumn;
 
     private RoomService roomService = RoomService.getInstance();
 
+    private ApplicationSettingsReader asr = new ApplicationSettingsReader();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        priceColumn.setCellValueFactory(
-                new PropertyValueFactory<>("pricePerNight")
+        priceColumn.setCellValueFactory((cellData) -> {
+                    double price = cellData.getValue().getPricePerNight() / CurrencyService.getCurrnecyPrice(asr.getCurrnecy());
+                    DecimalFormat df = new DecimalFormat("#.##");
+                    return new ReadOnlyStringWrapper(df.format(price) + " " + asr.getCurrnecy());
+                }
         );
 
         roomService.getRoomsList()
